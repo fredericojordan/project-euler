3eb925ddeeaa80b748c9f6eb14712d749a43e73a
Frederico Jordan <fredericojordan@gmail.com>
2018-04-13 01:34:19 -0300

    Making functions private

--- src/001/problem001.exs
+++ src/001/problem001.exs
@@ -1,8 +1,8 @@
 #!/usr/bin/env elixir
 defmodule Problem001 do
-  def multiples_sum(0), do: 0
-  def multiples_sum(x) when rem(x, 3) == 0 or rem(x, 5) == 0, do: x + multiples_sum(x-1)
-  def multiples_sum(x), do: multiples_sum(x-1)
+  defp multiples_sum(0), do: 0
+  defp multiples_sum(x) when rem(x, 3) == 0 or rem(x, 5) == 0, do: x + multiples_sum(x-1)
+  defp multiples_sum(x), do: multiples_sum(x-1)
 
   def solve, do: multiples_sum(999)
 end
