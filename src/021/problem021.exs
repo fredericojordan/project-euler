ee70e07806746a794a2d8e010202c89eb2d542fd
Frederico Jordan <fredericojordan@gmail.com>
2018-04-26 01:42:15 -0300

    Adding divisors(1) for completeness

--- src/021/problem021.exs
+++ src/021/problem021.exs
@@ -3,6 +3,7 @@ defmodule Problem021 do
   defp divisors(x, n, divisors) when n == x, do: divisors
   defp divisors(x, n, divisors) when rem(x, n) == 0, do: divisors(x, n+1, [n | divisors])
   defp divisors(x, n, divisors), do: divisors(x, n+1, divisors)
+  defp divisors(1), do: [1]
   defp divisors(x) when is_integer(x) and x > 0, do: divisors(x, 1, [])
   
   defp divisor_sum(x), do: divisors(x) |> Enum.sum()
@@ -10,7 +11,7 @@ defmodule Problem021 do
   defp is_amicable(x), do: x == divisor_sum(divisor_sum(x)) and x !== divisor_sum(x)
   
   def solve do
-    2..10000
+    1..10000
     |> Stream.filter(&is_amicable(&1))
     |> Enum.sum()
   end
