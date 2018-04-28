2e4b500edaa7afc63467294bb01fe30fc8984b93
Frederico Jordan <fredericojordan@gmail.com>
2018-04-28 20:36:20 -0300

    Adding problem 006

---
+++ src/006/problem006.exs
@@ -0,0 +1,23 @@
+#!/usr/bin/env elixir
+defmodule Problem006 do
+
+  defp square(x), do: x*x
+  
+  defp sum_of_squares(a, b) do
+    a..b
+    |> Stream.map(&square(&1))
+    |> Enum.sum()
+  end
+  
+  defp square_of_sum(a, b) do
+    a..b
+    |> Enum.sum()
+    |> square()
+  end
+
+  def solve do
+    square_of_sum(1,100) - sum_of_squares(1,100)
+  end
+end
+
+IO.puts Problem006.solve
