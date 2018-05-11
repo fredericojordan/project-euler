7702cd9e68ffbd0b9a983704af1a36a22ba913ea
Frederico Jordan <fredericojordan@gmail.com>
2018-05-11 00:34:11 -0300

    Optimizing isoceles triangles generator

--- src/138/problem138.exs
+++ src/138/problem138.exs
@@ -2,16 +2,16 @@
 defmodule Problem138 do
   defp special_isoceles_triangles_plus(n) when n > 0 do
     for a <- 1..n,
-        b <- a+1..n,
-        5*a*a  + 8*a + 4 == 4*b*b,
-        do: [b, a+1, a]
+        b = :math.sqrt(1.25*a*a  + 2*a + 1),
+        b == trunc(b),
+        do: [trunc(b), a+1, a]
   end
 
   defp special_isoceles_triangles_minus(n) when n > 0 do
     for a <- 1..n,
-        b <- a+1..n,
-        5*a*a  - 8*a + 4 == 4*b*b,
-        do: [b, a, a-1]
+        b = :math.sqrt(1.25*a*a  - 2*a + 1),
+        b == trunc(b),
+        do: [trunc(b), a, a-1]
   end
 
   defp special_isoceles_triangles(n) when n > 0 do
@@ -23,7 +23,7 @@ defmodule Problem138 do
   end
 
   def solve do
-    special_isoceles_triangles(10_000)
+    special_isoceles_triangles(500_000_000)
 #    |> Enum.map(&(List.first(&1)))
 #    |> Enum.sort()
 #    |> Enum.take(12)
