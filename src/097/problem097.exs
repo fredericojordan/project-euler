cce317dab50ff15cc0fdf8bfffc37b44337c847c
Frederico Jordan <fredericojordan@gmail.com>
2018-05-17 02:16:05 -0300

    Making fn private

--- src/097/problem097.exs
+++ src/097/problem097.exs
@@ -1,14 +1,14 @@
 #!/usr/bin/env elixir
 defmodule Problem097 do
 
-  def pow_mod10000000000(_, 0), do: 1
-  def pow_mod10000000000(x, n) when rem(n, 2) != 0 do
+  defp pow_mod10000000000(_, 0), do: 1
+  defp pow_mod10000000000(x, n) when rem(n, 2) != 0 do
     x * pow_mod10000000000(x, n - 1)
     |> Integer.digits()
     |> Enum.take(-10)
     |> Integer.undigits()
   end
-  def pow_mod10000000000(x, n) do
+  defp pow_mod10000000000(x, n) do
     result = pow_mod10000000000(x, div(n, 2))
     result * result
     |> Integer.digits()
